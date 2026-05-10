8afa578a721d4060f4cd73c010aea4c22a14c3b5
SiadIsmail <siadkml2007@gmail.com>
2026-05-10 13:42:01 +0200

    Added dynamic Buttons for Mood and Weight

--- Animal Diary App/Data/View/CalendarPage.xaml.cs
+++ Animal Diary App/Data/View/CalendarPage.xaml.cs
@@ -20,17 +20,17 @@ public partial class CalendarPage : ContentPage
 	}
 	private async void OnMoodEntryCompleted(object? sender, EventArgs e)
 	{
-		var viewModel = (CalendarViewModel)BindingContext;
-		await viewModel.SavePetMoodEntryAsync();
-		await viewModel.LoadEntriesAsync();
+		var vm = (CalendarViewModel)BindingContext;
+		vm.OnMoodEntryCompleted.Execute(null);
 	}
+
+
 	private async void OnWeightEntryCompleted(object? sender, EventArgs e)
 	{
-		var viewModel = (CalendarViewModel)BindingContext;
-		await viewModel.SavePetWeightEntryAsync();
-		await viewModel.LoadEntriesAsync();
+		var vm = (CalendarViewModel)BindingContext;
+		vm.OnWeightEntryCompleted.Execute(null);
 	}
-	
-	
+
+
 
 }
